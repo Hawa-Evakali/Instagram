7a103412ecc00d11f0d5815d5a65ba7614d49be7
Hawa-Evakali <hawa.evakali@student.moringaschool.com>
2022-04-07 01:12:33 +0300

    import user

--- app/tests.py
+++ app/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from .models import Image,Profile,Likes,Comments
-from django.contrib.auth.models import User
+from django.contrib.auth.models import Us
 
 # Create your tests here.
 class ImageTestCase(TestCase):
